8581865dac98face29e25764119420234f488c08
SirEAsternhouse <aso21arnold.oosthuizen@gmail.com>
2024-08-06 13:14:39 -0700

    task 9 -12

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,37 @@
 #!/usr/bin/python3
 """
-App module for API
+Flask application instance
 """
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-import os
 
 app = Flask(__name__)
 
-# Register the app_views blueprint
+# Register blueprint for app views
 app.register_blueprint(app_views)
+
+# Create a CORS instance allowing all origins
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Closes the storage on teardown"""
+    """Closes the database again at the end of the request."""
     storage.close()
 
 
 @app.errorhandler(404)
 def not_found(error):
-    """JSON-formatted 404 error response"""
+    """
+    Handler for 404 errors that returns a
+    JSON-formatted 404 status code response
+    """
     return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = os.getenv('HBNB_API_PORT', 5000)
-    app.run(host=host, port=port, threaded=True)
+    # Run the app on host 0.0.0.0 and port 5000
+    app.run(host="0.0.0.0", port=5000, threaded=True)
